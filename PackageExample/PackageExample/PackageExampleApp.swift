c6e0de29b477f0ef260e908e05aef47283af0436
Mohamed Dorgham <dorgham@ryets.com>
2022-06-20 02:37:50 +0200

    added Example

---
+++ PackageExample/PackageExample/PackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackageExampleApp.swift
+//  PackageExample
+//
+//  Created by mohamed dorgham on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
