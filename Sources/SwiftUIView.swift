180d59de35cc3d39cf966ba276a97cb7a718ad7c
Mohamed Dorgham <dorgham@ryets.com>
2022-06-20 02:29:13 +0200

    Initial commit

---
+++ Sources/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by mohamed dorgham on 20/06/2022.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct SwiftUIView: View {
+    var body: some View {
+        Text("First Package Test! ")
+    }
+}
+
+@available(iOS 13.0, *)
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
